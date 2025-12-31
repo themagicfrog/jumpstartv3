e86d9bb467bb05051c5f9ad83969f14146c2be92
estellagu <126267993+themagicfrog@users.noreply.github.com>
2025-12-30 21:06:24 -0500

    typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,8 +10,10 @@ export default function Footer() {
       <Image src="/assets/stripe-line.svg" alt="Stripe Line" width={2000} height={100} style={{ width: '100%', height: 'auto' }} />
       <div style={{ backgroundColor: '#071540', padding: '3rem', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '0.5rem' }}>
         <p style={{ color: 'white', textAlign: 'center', margin: 0, fontSize: '2rem' }}>
-          Made with &lt;3 by{' '}
-          <a href="https://estellagu.com" target="_blank" rel="noopener noreferrer" style={linkStyle}>Estella Gu</a>
+          Made with &lt;3 by Estella Gu
+        </p>
+        <p style={{ color: 'white', textAlign: 'center', margin: 0, fontSize: '2rem' }}>
+          <a href="https://github.com/themagicfrog" target="_blank" rel="noopener noreferrer" style={linkStyle}>Follow me on Github!</a>
         </p>
         <p style={{ color: 'white', textAlign: 'center', margin: 0, fontSize: '2rem' }}>
           <a href="https://hackclub.com" target="_blank" rel="noopener noreferrer" style={linkStyle}>Hack Club</a>
